3fba0cf7d1c3a45bc4665936b60136c35ccf2122
David Landy <dlandy@richmond.edu>
2013-01-28 17:57:00 -0500

    Still committing some initial Files.

---
+++ path analysis.R
@@ -0,0 +1,24 @@
+# requires ########################
+require(plyr)
+require(ggplot2)
+
+# Load MathsGarden Data ###########
+
+require(data.table)
+wd <- "/Users/dlandy/Dropbox/Projects/Math\ Garden/"
+setwd(wd)
+
+#source("/Users/dlandy/Dropbox/Operation Order (shared)/rekenvolgorde.r")
+source(paste(wd, "buildItems.R", sep=""))
+source(paste(wd, "loadMathGardenDataSets.R", sep="/"))
+
+# Load Data:
+outcome = loadMathGardenDataSets(0.0001)
+
+
+
+
+
+
+# Reduce to spacing cases
+
